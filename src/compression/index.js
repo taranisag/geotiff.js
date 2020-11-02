0cdcb99d06de51419ca30db00d9c13b3c2323c2e
Michael Frenkel <michael_frenkel@taranis.ag>
2020-11-02 16:39:58 +0200

    remove DeflateDecoder

--- src/compression/index.js
+++ src/compression/index.js
@@ -1,8 +1,7 @@
-import RawDecoder from './raw';
-import LZWDecoder from './lzw';
-import JpegDecoder from './jpeg';
-import DeflateDecoder from './deflate';
-import PackbitsDecoder from './packbits';
+import RawDecoder from "./raw";
+import LZWDecoder from "./lzw";
+import JpegDecoder from "./jpeg";
+import PackbitsDecoder from "./packbits";
 
 export function getDecoder(fileDirectory) {
   switch (fileDirectory.Compression) {
@@ -12,15 +11,15 @@ export function getDecoder(fileDirectory) {
     case 5: // LZW
       return new LZWDecoder();
     case 6: // JPEG
-      throw new Error('old style JPEG compression is not supported.');
+      throw new Error("old style JPEG compression is not supported.");
     case 7: // JPEG
       return new JpegDecoder(fileDirectory);
     case 8: // Deflate as recognized by Adobe
-    case 32946: // Deflate GDAL default
-      return new DeflateDecoder();
     case 32773: // packbits
       return new PackbitsDecoder();
     default:
-      throw new Error(`Unknown compression method identifier: ${fileDirectory.Compression}`);
+      throw new Error(
+        `Unknown compression method identifier: ${fileDirectory.Compression}`
+      );
   }
 }
